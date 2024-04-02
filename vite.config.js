15313f4ba514e5124fd699f3a524aba5d100aa13
Supreme Khadka <supremekhadka2@gmail.com>
2024-04-02 13:28:40 +0545

    file type change and vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-summarizer/'
+  base: '/ai-summarizer/',
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
