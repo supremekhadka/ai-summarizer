235685246ce98e080a79104e161016181bca5a50
Supreme Khadka <supremekhadka2@gmail.com>
2024-05-07 14:17:26 +0545

    added length feature and updated README.md

--- README.md
+++ README.md
@@ -1,8 +1,57 @@
-# React + Vite
+# AI - Summarizer
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This React application utilizes Rapid API's Article Extractor and Summarizer API to summarize articles.
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Summarize articles from any website.
+- View summarized content for quick insights.
+- Easy-to-use interface for efficient summarization.
+
+## Installation
+
+1. **Clone the repository:**
+   
+   ```
+   git clone https://github.com/supremekhadka/ai-summarizer.git
+   ```
+
+2. **Navigate into the project directory:**
+
+    ```
+    cd ai-summarizer
+    ```
+
+3. **Install dependencies:**
+
+    ```
+    npm install
+    ```
+
+4. **Create a .env file in the root directory and add your Rapid API key:**
+
+    ```
+    REACT_APP_RAPID_API_KEY=your-rapid-api-key
+    ```
+
+## Usage
+
+1. **Start the development server:**
+
+    ```
+    npm start
+    ```
+2. **Open your browser and navigate to http://localhost:3000.**
+3. **Enter the URL of the article you want to summarize (along with 'https://').**
+4. **Enter to get the summarized content.**
+
+## Technologies Used
+
+- React + Tailwindcss
+- Redux Toolkit
+- Rapid API
+- Axios
+
+## Demo
+
+You can find a live demo of this application [here](https://supremekhadka.github.io/ai-summarizer/).
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,6 +7,7 @@ import { useLazyGetSummaryQuery } from '../services/article'
 const Demo = () => {
   const [article, setArticle] = useState({
     url: '',
+    length: '5',
     summary: ''
   });
 
@@ -27,7 +28,7 @@ const Demo = () => {
   const handleSubmit = async(e) => {
     e.preventDefault();
 
-    const { data } = await getSummary({ articleUrl: article.url });
+    const { data } = await getSummary({ articleUrl: article.url, articleLength: article.length });
 
     if(data?.summary){
       const newArticle = { ...article, summary: data.summary };
@@ -46,38 +47,61 @@ const Demo = () => {
     setTimeout(() => setCopied(false), 3000);
   }
 
+  const handleLengthChange = (event) => {
+    setArticle({ ...article, length: event.target.value });
+  }
+
   return (
     <section className="mt-16 w-full max-w-2xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form 
           onSubmit={handleSubmit}  
-          className="relative flex justify-center items-center">
-            <img 
-              src={linkIcon} 
-              alt="Link Icon"
-              className="absolute left-0 my-2 ml-3 w-5" />
-            <input 
-              type="url"
-              placeholder="Enter a URL"
-              value={article.url}
-              onChange={(e) => setArticle({
-                ...article, url: e.target.value
-              })}
-              required
-              className='url_input peer'
+          className="flex flex-col justify-center items-center gap-10">
+            <div className="flex length-select w-full justify-center items-center gap-5">
+              <label htmlFor="length">Length:</label>
+              <input 
+                type="range" 
+                id="length" 
+                name="length" 
+                min="5" 
+                max="45" 
+                step="20" 
+                value={article.length} 
+                onChange={handleLengthChange} 
+                className='w-20 bg-violet-600'
+                style={{backgroundImage: 'linear-gradient(to right, var(--tw-gradient-stops))', backgroundColor: '#4F46E5' }}
+              />
+              <span className="length-label w-14 text-end">{article.length == 5 ? 'Short' : article.length == 25 ? 'Medium' : 'Long'}</span>
+            </div>
+            <div className="link flex relative w-full">
+              <img 
+                src={linkIcon} 
+                alt="Link Icon"
+                className="absolute left-0 my-4 ml-3 w-5" />
+                
+              <input 
+                type="url"
+                placeholder="Enter a URL"
+                value={article.url}
+                onChange={(e) => setArticle({
+                  ...article, url: e.target.value
+                })}
+                required
+                className='url_input peer'
               />
 
-              <button
-                type='submit'
-                className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
-              >
-                <img 
-                  src={submit} 
-                  alt="submit_icon" 
-                  className='h-5'
-                />
-              </button>
+                <button
+                  type='submit'
+                  className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+                >
+                  <img 
+                    src={submit} 
+                    alt="submit_icon" 
+                    className='h-5'
+                  />
+                </button>
+            </div>
           </form>
 
           <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
